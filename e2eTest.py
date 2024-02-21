18c4a91a6938a485b2d3341de9ac18c09c41e81c
cherub randhawa <cherubs1897@gmail.com>
2024-02-20 21:18:29 -0500

    solved elements bug, selector bug and driver bug

--- e2eTest.py
+++ e2eTest.py
@@ -9,18 +9,18 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 service_obj = Service()
-driver = webdriver.chrome(service = service_obj)
+driver = webdriver.Chrome(service = service_obj)
 driver.implicitly_wait(4)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()   ## xpath is //a[contains(@href,'shop')]
-products = driver.find_element(By.XPATH, "//div[@class = 'card h-100']")
+products = driver.find_elements(By.XPATH, "//div[@class = 'card h-100']")
 
 for product in products:
     productName = product.find_element(By.XPATH, "div/h4/a").text
     if productName == "Blackberry":
         product.find_element(By.XPATH, "div/button").click()
 
-driver.find_element(By.CSS_SELECTOR, "a[class*=''btn-primary]").click()
+driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
 driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
 driver.find_element(By.ID,"country").send_keys("ind")
 wait = WebDriverWait(driver, 10)
